0b26f5f4ad3b8c362bad3c7b32c26c0f2ef79cc7
Rubén Falcón <ruben@falcon.ar>
2023-03-31 11:17:42 -0300

    Adición de js para ajuste de ancho de la grilla. Versión 0.1

---
+++ app/app.js
@@ -0,0 +1,13 @@
+function actualizarAnchoDeCaja() {
+    let anchoDeVentana = window.innerWidth;
+    const caja = document.getElementById('contenedor-grilla');
+    anchoDeVentana = anchoDeVentana - 58;
+    caja.style.width = `${anchoDeVentana}px`;
+  }
+  
+  // Ejecuta la función al cargar la página
+  actualizarAnchoDeCaja();
+  
+  // Ejecuta la función cada vez que se redimensiona la ventana del navegador
+  window.addEventListener('resize', actualizarAnchoDeCaja);
+  
