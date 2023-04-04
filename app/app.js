8c188207313eb7e185feb385e6a802bc80a938da
Rubén Falcón <ruben@falcon.ar>
2023-04-04 15:41:41 -0300

    Prueba en celular sobre cambio de orientación.

--- app/app.js
+++ app/app.js
@@ -1,8 +1,20 @@
-function actualizarAnchoDeCaja() {
-    let anchoDeVentana = window.innerWidth;
+function actualizarAnchoDeCaja(boolchangeorientation = false) {
+
+  window.t = undefined;
+
+  if (boolchangeorientation) {
+
+    setTimeout('console.log("cambió la orientación");', 250);
+      
+  }
+
+
+  let anchoDeVentana = window.innerWidth;
     const caja = document.getElementById('contenedor-grilla');
     anchoDeVentana = anchoDeVentana - 58;
     caja.style.width = `${anchoDeVentana}px`;
+
+
   }
   
   // Ejecuta la función al cargar la página
@@ -10,5 +22,5 @@ function actualizarAnchoDeCaja() {
   
   // Ejecuta la función cada vez que se redimensiona la ventana del navegador
   window.addEventListener('resize', actualizarAnchoDeCaja);
-  window.addEventListener('orientationchange', actualizarAnchoDeCaja);
+  window.addEventListener('orientationchange', actualizarAnchoDeCaja(true));
   
