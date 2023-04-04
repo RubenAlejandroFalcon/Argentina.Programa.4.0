a521cf4995e504ee07a3a6c46e8b49beee65a50f
Rubén Falcón <ruben@falcon.ar>
2023-04-04 16:13:42 -0300

    Prueba 5 por el cambio de orientación.

--- app/app.js
+++ app/app.js
@@ -6,19 +6,59 @@ function actualizarAnchoDeCaja() {
   caja.style.width = `${anchoDeVentana}px`;
 }
   
+function orientation_changed ()
+{
+    if ( is_portrait() )
+    {
+      actualizarAnchoDeCaja();
+    }
+    else if ( is_landscape() )
+    {
+      actualizarAnchoDeCaja();
+    }
+    clearTimeout(window.t);
+    delete window.t;
+}
 window.t = undefined;
 window.onorientationchange = function (event)
 {
-    window.t = setTimeout('actualizarAnchoDeCaja();', 2500);
-    this.alert("Cambio de orientación");
+    window.t = setTimeout('orientation_changed();', 250);
+}
+function is_landscape()
+{
+    var uagent = navigator.userAgent.toLowerCase();
+    if ( uagent.search('ipad') > -1 )
+    {
+        var r = ( window.orientation == 90 || window.orientation == -90 );
+    }
+    else
+    {
+        var r = ( screen.width > screen.height );
+    }
+    return r;
+}
+function is_portrait()
+{
+    var uagent = navigator.userAgent.toLowerCase();
+    if ( uagent.search('ipad') > -1 )
+    {
+        var r = ( window.orientation == 0 || window.orientation == 180 );
+    }
+    else
+    {
+        var r = ( screen.width < screen.height );
+    }
+    return r;
 }
 
-  // Ejecuta la función al cargar la página
-  actualizarAnchoDeCaja();
-  
-  // Ejecuta la función cada vez que se redimensiona la ventana del navegador
-  window.addEventListener('resize', actualizarAnchoDeCaja);
 
-  // Ejecuta la función cada vez que se cambia la orientación en el celular
-  //window.addEventListener('orientationchange', actualizarAnchoDeCaja);
+
+// Ejecuta la función al cargar la página
+actualizarAnchoDeCaja();
+  
+// Ejecuta la función cada vez que se redimensiona la ventana del navegador
+window.addEventListener('resize', actualizarAnchoDeCaja);
+
+// Ejecuta la función cada vez que se cambia la orientación en el celular, pero no tiene efecto
+//window.addEventListener('orientationchange', actualizarAnchoDeCaja);
   
